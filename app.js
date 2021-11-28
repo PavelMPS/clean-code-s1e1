01921ad89396836223a3989ecb2a462bd3fa87ec
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-11-28 23:41:35 +0300

    refactor: adjust HTML-formatting according to rule 2.5 in html-and-css.md

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ var createNewTaskElement=function(taskString){
     listItem.appendChild(editInput);
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
-    listItem.className="task-list-item";
+    listItem.className="main__task-list-item";
     return listItem;
 }
 
@@ -88,7 +88,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("task-list-item_edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -102,7 +102,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("task-list-item_edit-mode");
 };
 
 
